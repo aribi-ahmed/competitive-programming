7b44651144c64bb2d6b60895ba0863bed68a077c
Ahmed Aribi <aribi.ahmed@outlook.com>
2025-08-11 20:21:55 +0100

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// -------------------- DEBUGGING --------------------
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << "\n";
+#else
+#define debug(x)
+#endif
+
+// -------------------- FAST I/O --------------------
+ios_base::sync_with_stdio(false);
+cin.tie(nullptr);
+
+// -------------------- TYPE ALIASES --------------------
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<long long,long long>;
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+
+// -------------------- CONSTANTS --------------------
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+
+// -------------------- MAIN --------------------
+int main() {
+    // your code here
+    return 0;
+}
