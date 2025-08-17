512e66315534366f3041f978ab463609c36ee05e
Ahmed Aribi <aribi.ahmed@outlook.com>
2025-08-17 23:42:58 +0100

    Create swap.cpp

---
+++ Month 1/Review/Functions/swap.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+    int a, b; cin >> a >> b;
+    cout << b << " " << a << endl;
+}
+ 
+int main() {
+    solve();
+    return 0;
+}
